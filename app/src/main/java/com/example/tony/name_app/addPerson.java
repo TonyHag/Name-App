1137072eca0f738cd34c59a6408fa63c353c4c94
TonyHag <to_hager@hotmail.com>
2017-01-12 14:55:10 +0100

    Lagt til addperson activity

---
+++ app/src/main/java/com/example/tony/name_app/addPerson.java
@@ -0,0 +1,13 @@
+package com.example.tony.name_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_person);
+    }
+}
