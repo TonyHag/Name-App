5784792c186b56c0d2a4d50e207e3d11fffbe16b
kimrunar <kimrunar_3@hotmail.com>
2017-01-12 17:07:54 +0100

    camera

--- app/src/main/java/com/example/tony/name_app/addPerson.java
+++ app/src/main/java/com/example/tony/name_app/addPerson.java
@@ -1,5 +1,7 @@
 package com.example.tony.name_app;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,7 +13,17 @@ public class addPerson extends AppCompatActivity {
         setContentView(R.layout.activity_add_person);
     }
     public void add(){
+       dispatchTakePictureIntent();
 
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 
