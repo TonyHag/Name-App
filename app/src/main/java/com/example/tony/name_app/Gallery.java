d274653e623ecdedec4c5920a9afb1bd34dd9a6f
Nataniel Pedersen <natanielmp@gmail.com>
2017-01-23 01:36:10 +0100

    Gallery work more

--- app/src/main/java/com/example/tony/name_app/Gallery.java
+++ app/src/main/java/com/example/tony/name_app/Gallery.java
@@ -2,10 +2,14 @@ package com.example.tony.name_app;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,54 +25,26 @@ public class Gallery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
         persons = getList();
-        ImageView img1 = (ImageView) this.findViewById(R.id.img1);
-        ImageView img2 = (ImageView) this.findViewById(R.id.img2);
-        ImageView img3 = (ImageView) this.findViewById(R.id.img3);
-        ImageView img4 = (ImageView) this.findViewById(R.id.img4);
-        ImageView img5 = (ImageView) this.findViewById(R.id.img5);
-        if(!persons.isEmpty()) {
-            Bitmap photo1 = persons.get(0).getBitmap();
-            img1.setImageBitmap(photo1);
-        }
-        if(persons.size() >= 2) {
-            Bitmap photo2 = persons.get(1).getBitmap();
-            img2.setImageBitmap(photo2);
-        }
-        if(persons.size() >= 3) {
-            Bitmap photo3 = persons.get(2).getBitmap();
-            img3.setImageBitmap(photo3);
-        }
-        if(persons.size() >= 4) {
-            Bitmap photo4 = persons.get(3).getBitmap();
-            img4.setImageBitmap(photo4);
-        }
-        if(persons.size() >= 5) {
-            Bitmap photo5 = persons.get(4).getBitmap();
-            img5.setImageBitmap(photo5);
-        }
-    }
-    public void gotoPicture(View view){
-        Intent intent = new Intent(this, ViewPicture.class);
-        switch (view.getId()) {
-            case R.id.img1:
-                intent.putExtra("index", 0);
-                break;
-            case R.id.img2:
-                intent.putExtra("index", 1);
-                break;
-            case R.id.img3:
-                intent.putExtra("index", 2);
-                break;
-            case R.id.img4:
-                intent.putExtra("index", 3);
-                break;
-            case R.id.img5:
-                intent.putExtra("index", 4);
-                break;
 
-        }
-        startActivity(intent);
+        GridView grid = (GridView) findViewById(R.id.gridView);
+        grid.setAdapter(new ImageAdapter(this, R.layout.single_image, persons));
 
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            long mLastClickTime = 0;
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
+                    return;
+                }
+
+                mLastClickTime = SystemClock.elapsedRealtime();
+
+                Toast.makeText(Gallery.this, "" +  persons.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/tony/name_app/ImageAdapter.java
@@ -0,0 +1,47 @@
+package com.example.tony.name_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nataniel on 23.01.2017.
+ */
+
+public class ImageAdapter extends ArrayAdapter<Person> {
+
+    private Context context;
+
+    public ImageAdapter(Context context, int resource, ArrayList<Person> objects) {
+        super(context, resource, objects);
+        this.context = context;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(this.getContext())
+                    .inflate(R.layout.single_image, parent, false);
+
+            holder = (ImageView) convertView.findViewById(R.id.imageView);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ImageView) convertView.getTag();
+        }
+
+        Person item = getItem(position);
+        if (item != null) {
+            holder.setImageBitmap(item.getBitmap());
+            return convertView;
+        }
+
+        return convertView;
+    }
+}
